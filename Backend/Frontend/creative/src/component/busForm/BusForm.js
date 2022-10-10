faaa0a3f1d53d4e07d18340943113344387ca153
KYUTAE PARK <rbxo0109@gmail.com>
2022-10-10 22:31:02 +0900

    Feat: BusForm.js updata
    * 버스 Form 업데이트와 feature 브런치 추가를 위한 commit

--- Backend/Frontend/creative/src/component/busForm/BusForm.js
+++ Backend/Frontend/creative/src/component/busForm/BusForm.js
@@ -1,11 +1,35 @@
 import BusButton from "./BusButton";
 import BusInput from "./BusInput";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+    display:flex;
+`
+
 
 const BusForm = () => {
-    return (<div>
-        <BusButton />
-        <BusInput />
-    </div>
+    const BusStationData = (event) => {
+        event.preventDefault();
+        const { target: [input] } = event
+        const { value } = input
+        input.value = "";
+        axios.get(`/station/${value}`, {
+
+        }).then(res => {
+            const { data } = res;
+            console.log(data)
+
+        }).catch(error => {
+            alert("데이터를 받아오지 못했습니다.")
+        });
+
+    }
+
+    return (
+        <StyledForm>
+            <BusButton />
+            <BusInput />
+        </StyledForm>
     )
 }
 
