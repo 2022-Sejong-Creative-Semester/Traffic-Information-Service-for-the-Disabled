a0baaba60c7130200a929ef73da84443798cb6ef
KYUTAE PARK <rbxo0109@gmail.com>
2022-10-27 22:23:26 +0900

    feat: 저상버스 리스트 해결
    * 저상버스에 검색을 이용 시 저상버스의 정보들을 리스트에 보여주었습니다.

--- Backend/Frontend/creative/src/component/bus-component/buslist/BusList.js
+++ Backend/Frontend/creative/src/component/bus-component/buslist/BusList.js
@@ -2,27 +2,35 @@ import styled from "styled-components";
 import BusItem from "./BusItem";
 import StationItem from "./StationItem"
 import { useSelector } from "react-redux";
+import test from "./test.json"
 
 const StyledList = styled.ul`
+display:flex;
+flex-direction:column;
+align-items: center;
 box-sizing: border-box;
 width: 744px;
 height: 590px;
 background: #FFFFFF;
 border: 4px solid #CDD029;
+padding:0;
 `
 
 const BusList = () => {
     const station = useSelector(state => state.bus.station)
-    console.log(station)
+    const { elements } = station
     const stationCheck = useSelector(state => state.bus.stationCheck)
     return (
         <StyledList>
-            {stationCheck && station.forEach(element => {
+            {stationCheck && elements.map(element => (
                 <StationItem
-
-
+                    key={element.elements[0].elements[0].text}
+                    items={{
+                        stationId: element.elements[0].elements[0].text,
+                        stationName: element.elements[4].elements[0].text
+                    }}
                 />
-            })}
+            ))}
         </StyledList>
     )
 }
