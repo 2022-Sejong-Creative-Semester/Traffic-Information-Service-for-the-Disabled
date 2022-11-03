2a4668b894fc8f3c3ba76db09bb61378e7e6e0c6
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-04 01:31:37 +0900

    Fix: 에러 및 예외 처리 기능 추가
    try-catch 문을 이용하여 Exception Error 및 예외 처리 기능 추가
    
    Resovled: #35

--- Backend/routes/trafficdata.js
+++ Backend/routes/trafficdata.js
@@ -17,93 +17,112 @@ function getTraffic(callback) {
 }
 
 function getStation(stNm, callback) {
-	const url = 'http://ws.bus.go.kr/api/rest/stationinfo/getLowStationByName';
-	let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey.serviceKey;
-	queryParams += '&' + encodeURIComponent('stSrch') + '=' + encodeURIComponent(stNm);
-	//console.log(url+queryParams);
-	//let StationList = [];
+	try{
+		const url = 'http://ws.bus.go.kr/api/rest/stationinfo/getLowStationByName';
+		let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey.serviceKey;
+		queryParams += '&' + encodeURIComponent('stSrch') + '=' + encodeURIComponent(stNm);
+		//console.log(url+queryParams);
+		//let StationList = [];
 
-	return request({
-		url: url + queryParams,
-		method: 'GET'
-	}, function (error, response, body) {
-		//console.log('Reponse received', body);
-		const parseJson = convert.xml2json(body);
-		const stationinfo = JSON.parse(parseJson).elements[0].elements[2];
-		const stationlength = stationinfo.elements.length;
-		//console.log(stationinfo.elements[0]);
-		let stationRes = [];
+		return request({
+			url: url + queryParams,
+			method: 'GET'
+		}, function (error, response, body) {
+			//console.log('Reponse received', body);
+			const parseJson = convert.xml2json(body);
+			const stationinfo = JSON.parse(parseJson).elements[0].elements[2];
+			const stationlength = stationinfo.elements.length;
+			//console.log(stationinfo.elements[0]);
+			let stationRes = [];
 
-		for(let i=0;i<stationlength;i++){
-			const arsId = stationinfo.elements[i].elements[0].elements[0].text;
-			const stId = stationinfo.elements[i].elements[3].elements[0].text;
-			const stNm = stationinfo.elements[i].elements[4].elements[0].text;
-			const tmX = stationinfo.elements[i].elements[5].elements[0].text;
-			const tmY = stationinfo.elements[i].elements[6].elements[0].text;
-			stationRes.push({
-				arsId: arsId,
-				stId: stId,
-				stNm: stNm,
-				tmX: tmX,
-				tmY: tmY,
-			})
-		}
+			for(let i=0;i<stationlength;i++){
+				const arsId = stationinfo.elements[i].elements[0].elements[0].text;
+				const stId = stationinfo.elements[i].elements[3].elements[0].text;
+				const stNm = stationinfo.elements[i].elements[4].elements[0].text;
+				const tmX = stationinfo.elements[i].elements[5].elements[0].text;
+				const tmY = stationinfo.elements[i].elements[6].elements[0].text;
+				stationRes.push({
+					arsId: arsId,
+					stId: stId,
+					stNm: stNm,
+					tmX: tmX,
+					tmY: tmY,
+				})
+			}
 
-		//console.log('Json', stationinfo);
-		//console.log(stationinfo.elements[0].elements[2]);
-		callback(stationRes);
-	});
+			//console.log('Json', stationinfo);
+			//console.log(stationinfo.elements[0].elements[2]);
+			callback(stationRes);
+		});
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({
+			error: e,
+			errorString: e.toString(),
+		});
+	}
+	
 
 }
 
 function getStationInfo(arsId,callback){
-	const url = 'http://ws.bus.go.kr/api/rest/stationinfo/getLowStationByUid';
-	let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey.serviceKey;
-	queryParams += '&' + encodeURIComponent('arsId') + '=' + arsId;
+	
+	try{
+		const url = 'http://ws.bus.go.kr/api/rest/stationinfo/getLowStationByUid';
+		let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey.serviceKey;
+		queryParams += '&' + encodeURIComponent('arsId') + '=' + arsId;
 
-
-
-	return request({
-		url: url + queryParams,
-		method: 'GET'
-	}, function(error,response,body){
+		return request({
+			url: url + queryParams,
+			method: 'GET'
+		}, function(error,response,body){
 		
-		const parseJson = convert.xml2json(body);
-		const stationinfo = JSON.parse(parseJson).elements[0].elements[2];
+			const parseJson = convert.xml2json(body);
+			const stationinfo = JSON.parse(parseJson).elements[0].elements[2];
 		
-		if(stationinfo.elements == null){
-			callback(0);
-		}
+			if(stationinfo.elements == null){
+				callback(0);
+			}
 
-		const buslength = stationinfo.elements.length;
+			const buslength = stationinfo.elements.length;
 
-		let busInfo = [];
+			let busInfo = [];
 
-		for(let i = 0; i < buslength; i++){
-			const busrouteid = stationinfo.elements[i].elements[5].elements[0].text;
-			const busrouteAbrv = stationinfo.elements[i].elements[4].elements[0].text;
-			const bustype = stationinfo.elements[i].elements[6].elements[0].text;
-			const adirection = stationinfo.elements[i].elements[0].elements[0].text;
-			const congestion = stationinfo.elements[i].elements[9].elements[0].text;
-			const nxtStn = stationinfo.elements[i].elements[22].elements[0].text;
-			const arrmsg1 = stationinfo.elements[i].elements[1].elements[0].text;
+			for(let i = 0; i < buslength; i++){
+				const busrouteid = stationinfo.elements[i].elements[5].elements[0].text;
+				const busrouteAbrv = stationinfo.elements[i].elements[4].elements[0].text;
+				const bustype = stationinfo.elements[i].elements[6].elements[0].text;
+				const adirection = stationinfo.elements[i].elements[0].elements[0].text;
+				const congestion = stationinfo.elements[i].elements[9].elements[0].text;
+				const nxtStn = stationinfo.elements[i].elements[22].elements[0].text;
+				const arrmsg1 = stationinfo.elements[i].elements[1].elements[0].text;
 			
-			busInfo.push({
-				busrouteid: busrouteid,
-				busrouteAbrv: busrouteAbrv,
-				bustype: bustype,
-				adirection: adirection,
-				congestion: congestion,
-				nxtStn: nxtStn,
-				arrmsg1: arrmsg1,
-			})
-		}
+				busInfo.push({
+					busrouteid: busrouteid,
+					busrouteAbrv: busrouteAbrv,
+					bustype: bustype,
+					adirection: adirection,
+					congestion: congestion,
+					nxtStn: nxtStn,
+					arrmsg1: arrmsg1,
+				})
+			}
 
-		//console.log(stationinfo);
+			//console.log(stationinfo);
 		
-		callback(busInfo);
+			callback(busInfo);
 		
-	});
+		});
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({
+			error: e,
+			errorString: e.toString(),
+		});
+	}
+	
 }
 
 
@@ -131,12 +150,22 @@ router.get('/station', async (req, res) => {
 
 router.get('/station/:stNm', async (req, res) => {
 
-	const stNm = req.params.stNm;
-	//console.log("station");
-	await getStation(stNm, station => {
-		//console.log(station);
-		return res.json(station);
-	})
+	try{
+		const stNm = req.params.stNm;
+		//console.log("station");
+		await getStation(stNm, station => {
+			//console.log(station);
+			return res.json(station);
+		})
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({
+			error: e,
+			errorString: e.toString(),
+		});
+	}
+	
 
 })
 
@@ -157,20 +186,39 @@ router.get('/stationInfo/:arsId', async (req, res) => {
 
 	const arsId = req.params.arsId;
 	//console.log("station");
-	await getStationInfo(arsId, stationinfo => {
-		//console.log(station);
-		if(stationinfo == 0){
-			return res.status(404).json({
-				error: 'No Bus In Station'
-			})
-		}
-		else 
-			return res.json(stationinfo);
-	});
+	try{
+		await getStationInfo(arsId, stationinfo => {
+			//console.log(station);
+			if(stationinfo == 0){
+				return res.status(404).json({
+					error: 'No Bus In Station'
+				})
+			}
+			else 
+				return res.json(stationinfo);
+		});
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({
+			error: e,
+			errorString: e.toString(),
+		});
+	};
+	
 })
 
 router.get('/GPS',async (req,res)=>{
-	return res.json(gpsdata);
+	try{
+		return res.json(gpsdata);
+	}
+	catch(e){
+		console.error(e);
+		return res.status(500).json({
+			error: e,
+			errorString: e.toString(),
+		});
+	}
 })
 
 module.exports = router;
