d2b0050a6f45094e1792a6b4295ca7b8f0becc6d
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-15 22:51:25 +0900

    Feat: 철도 관련 정보 엑셀 추가 (#52)
    철도 관련 정보 저장된 엑셀 파일 추가 및 MySQL 연결용 js 파일 추가

---
+++ Backend/DBconnect.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const mysql = require('mysql');
+
+const conn {
+    
+}
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${port}`);
+})
