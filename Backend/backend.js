726dc68f1ebde08b6b94fd7c19e50fafec364d9f
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-23 16:11:48 +0900

    Modify: VM 서버 올리기 위해서 빌드 코드 주석처리
    빌드 코드 주석처리 및 창립제 발표 자료 추가

--- Backend/backend.js
+++ Backend/backend.js
@@ -8,17 +8,17 @@ const testRouter = require('./routes/test');
 const busRouter = require('./routes/busdata');
 const subwayRouter = require('./routes/subwaydata');
 
-app.use(express.static(path.join(__dirname, './Frontend/creative/build')))
+//app.use(express.static(path.join(__dirname, './Frontend/creative/build')))
 
 app.use('', testRouter);
 app.use('/bus', busRouter);
 app.use('/subway', subwayRouter);
 
-
+/*
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './Frontend/creative/build/index.html'));
 })
-
+*/
 
 app.get('/', (req, res) => {
     res.json({
