2fcf7f4b847a3c29a8ec40b8e73662e8b0231c22
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-27 00:00:08 +0900

    Feat: DB 연결 코드 추가, 외부 접속용 cors 추가
    MySQL 연결 코드 DBconnect.js 추가
    외부 IP 접속용 cors 추가

--- Backend/DBconnect.js
+++ Backend/DBconnect.js
@@ -4,9 +4,16 @@ const port = 3000;
 const path = require('path');
 const mysql = require('mysql');
 
-const conn {
-    
-}
+const conn = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '11111111',
+    database: 'interface_chatbot'
+};
+
+let connection = mysql.createConnection(conn); // DB 目池记 积己
+connection.connect();   // DB 立加
 
 app.get('/', (req, res) => {
     res.json({
--- Backend/backend.js
+++ Backend/backend.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3005;
 const path = require('path');
-
+const cors = require('cors');
 
 const testRouter = require('./routes/test');
 const busRouter = require('./routes/busdata');
 const subwayRouter = require('./routes/subwaydata');
+
+let corsOptions = {
+    origin: ['http://172.30.1.35:5000', 'http://game.jerrykang.com', 'http://admin.jerrykang.com', 'http://localhost:5000'],
+    credentials: true
+}
+
+app.use(cors(corsOptions));
 
 //app.use(express.static(path.join(__dirname, './Frontend/creative/build')))
 
