c817b5419d53d12ff3018db8ad47c8a5669204a3
KYUTAE PARK <rbxo0109@gmail.com>
2023-11-07 20:20:45 +0900

    feat:menuBar 수정(SVG로 표현)

--- Backend/Frontend/creative/src/component/menu/MenuBar.tsx
+++ Backend/Frontend/creative/src/component/menu/MenuBar.tsx
@@ -1,27 +1,46 @@
 import React from "react"
-import MenuBusButton from "./MenuBusButton.tsx"
-import MenuSubwayButton from "./MenuSubwayButton.tsx"
 import styled from "styled-components"
+
+import {ReactComponent as HOME} from "./menuSvg/HOME.svg";
+import {ReactComponent as BUS} from "./menuSvg/BUS.svg";
+import {ReactComponent as SUBWAY} from "./menuSvg/SUBWAY.svg";
+import {ReactComponent as SIGN} from "./menuSvg/SIGN.svg";
+
+
+
 
 const StyleMenuBar = styled.div`
 display:flex;
 position: fixed;
 bottom: 0;
-justify-content: space-between;
+justify-content: space-evenly;
+align-items: center;
 width: 100%;
+height: 8vh;
 background-color: #FFD12D;
-height: 10vh;
 text-decoration:none;
 @media (max-width:500px){
     justify-content:  space-evenly;
 }
 `
 
+const StyleButton = styled.button`
+    background-color: transparent;
+    border: 0;
+    .menu_img{
+        fill: white;
+        min-height: 4vh;
+    }
+`
+
 
 const MenuBar = () => {
     return (
         <StyleMenuBar>
-            
+            <StyleButton><HOME fill="white"  height="4vh"/></StyleButton>
+            <StyleButton><BUS fill="white"  height="4vh"/></StyleButton>
+            <StyleButton><SUBWAY fill="white"  height="4vh"/></StyleButton>
+            <StyleButton><SIGN fill="white"  height="4vh"/></StyleButton>
         </StyleMenuBar>
     )
 }
