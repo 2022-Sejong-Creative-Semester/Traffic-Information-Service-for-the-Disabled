1cce4fbde81106c048c069935df237046c72a59c
KYUTAE PARK <rbxo0109@gmail.com>
2023-11-16 02:34:11 +0900

    feat: busmode 수정

--- Backend/Frontend/creative/src/page/BusPage.tsx
+++ Backend/Frontend/creative/src/page/BusPage.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import Mapping from "../component/map/Mapping.tsx"
 import classes from "./BusPage.module.css"
 import Header from "../component/header/Header.tsx"
 import MenuBar from "../component/menu/MenuBar.tsx";
 import BusForm from "../component/bus-component/busform/BusForm.tsx"
 import BusList from "../component/bus-component/buslist/BusList.tsx"
+import { RootState } from "../store/index";
 
 const BusPage = () => {
+    const busmode = useSelector((state:RootState) => state.map.busmode)
     return (
         <div className={classes.buspage}>
             <Header />
             <Mapping />
             <BusForm />
-            <BusList/>
+            {busmode&&<BusList/>}
             <MenuBar/>
         </div>
     )
